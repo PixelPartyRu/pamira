172b4298f0893ea2b95db943a6ecf46013fba4aa
George Bramus <george.bramus@gmail.com>
2016-09-05 15:21:42 +0300

    Убрал лишние пробелы в файле Banner.php

--- app/Banner.php
+++ app/Banner.php
@@ -6,13 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Banner extends Model {
 
-    protected $table = 'banners';
-    protected $fillable = array("title","main_page");
-    
-    public function getUrl() {
+  protected $table = 'banners';
+  protected $fillable = array("title","main_page");
+
+  public function getUrl() {
     if (!preg_match("~^(?:f|ht)tps?://~i", $this->url)) {
-        $this->url = "http://" . $this->url;
+      $this->url = "http://" . $this->url;
     }
     return $this->url;
-    }
+  }
 }
