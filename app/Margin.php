d1f9cf8b9b4ea5960832e5638cdbf7998a9dc6ee
George Bramus <george.bramus@gmail.com>
2016-09-11 21:28:04 +0300

    Все ветки смешались, всё в голове смешалось, стараюсь непотеряться

--- app/Margin.php
+++ app/Margin.php
@@ -15,11 +15,11 @@ class Margin extends Model
   static $TYPE = array("wholesale","retail");
   static $TYPE_RUS = array("wholesale" => "Оптовая","retail" => "Розничная");
   static $validate_fields = array("name");
-  
+
   public function brands() {
            return $this->hasMany("App\MarginBrand","margin_id","id");
   }
-  
+
   public static function saveFormData($margin, $brand_id_values, $brand_margin_values, $default) {
 
         $validator = Validator::make($margin,array('name' => array('required', 'min:5')));
@@ -28,7 +28,7 @@ class Margin extends Model
             return false;
         }
         //var_dump($validator->fails());
-        
+
         if( isset( $margin['id'] ) ) {
             $margin_ob = self::find( intval($margin['id']) );
             $margin_ob->name = $margin['name'];
@@ -40,42 +40,43 @@ class Margin extends Model
             }
         }
         else {
-        $margin_ob = self::create($margin);
-        $margin_ob->user_id = Dealer::getLoginDealer()->id;
+            $margin_ob = self::create($margin);
+            $margin_ob->user_id = Dealer::getLoginDealer()->id;
         }
-        
+
         $margin_ob->save();
 
        /// dd($brand_id_values);
       // dd_not_die($brand_id_values);
       // dd_not_die($brand_margin_values);
-       
+
         foreach ($brand_id_values as $key => $val) {
-            
-            
+
+
             //dd_not_die($val);
-            
+
             $val = floatval( str_replace(",", ".", $val));
             //dd_not_die($val);
-            
+
             $margin_brand = new \App\MarginBrand(array(
                 "margin_id" => $margin_ob->id,
                 "brand_id" => $brand_margin_values[$key],
                 "margin" => $val
                     ));
-            
+
             $margin_ob->brands()->save($margin_brand);
         }
-        
+
         return true;
         //dd();
     }
-    
 
-    
+
+
     public static function formatPostMarginFormData(&$post) {
         $brand_margin = isset($post['brand_margin']) ? $post['brand_margin'] : array();
         $brand_ids = isset($post['brand_id']) ? $post['brand_id'] : array();
+        // $mark_up_initially = isset($post['mark_up_initially']) ? $post['mark_up_initially'] : "";
         unset($post['brand_margin']);
         unset($post['margin_for_all']);
         unset($post['_token']);
@@ -83,17 +84,16 @@ class Margin extends Model
         $format_data['margin'] = $post;
         $format_data['brand_margin'] = $brand_margin;
         $format_data['brand_ids'] = $brand_ids;
+        // $format_data['mark_up_initially'] = $mark_up_initially;
         return $format_data;
-        
-        
     }
-    
-    
+
+
     private static function setDefaultCallback(&$margin) {
         if ($margin->default == 1) {
             $auth_dealer = Dealer::getLoginDealer();
             $auth_dealer->setDefaultMargin($margin->type,$margin->id);
-            
+
         }
         //var_dump($margin);
     }
