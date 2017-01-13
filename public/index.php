6cb01f0de3c3fcafc37ad0a59c330d0d59eacca7
George Bramus <george.bramus@gmail.com>
2017-01-13 20:51:46 +0300

    commit on server-test after

--- public/index.php
+++ public/index.php
@@ -18,7 +18,7 @@
 | loading any of our classes later on. It feels nice to relax.
 |
 */
-
+//error_reporting(A_ALL); ini_set('display_errors', 'on');
 require __DIR__.'/../bootstrap/autoload.php';
 
 /*
