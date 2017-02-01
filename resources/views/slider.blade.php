f3647c257c57b7fb1514ac7e362f1d6abdf83938
George Bramus <george.bramus@gmail.com>
2017-02-01 07:59:34 +0300

    final slider-center

--- resources/views/slider.blade.php
+++ resources/views/slider.blade.php
@@ -1,5 +1,5 @@
 <!--Слайдер с адресами-->
-            <div class="row back">            
+            <div class="row back">
                 <div class="row-slider-info">
 
 <!--                    <div class="slider-info-block">
@@ -48,7 +48,7 @@
                                 <!--
                                 <li data-transition="slideleft" data-slotamount="5" data-masterspeed="700" data-delay="4000">
                                     <img src="/img/slider/new_slider/2.png" alt="slide7" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat">
-                                </li>                                
+                                </li>
 
                                 <li data-transition="slideleft" data-slotamount="5" data-masterspeed="700" data-delay="4000">
                                     <img src="/img/slider/new_slider/3.png"  alt="slide8" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat">
@@ -59,19 +59,19 @@
                                 -->
                                 <li data-transition="slideleft" data-slotamount="5" data-masterspeed="700" data-delay="4000">
                                     <img src="/img/slider/001.png" alt="slide7" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat">
-                                </li> 
+                                </li>
                                 <li data-transition="slideleft" data-slotamount="5" data-masterspeed="700" data-delay="4000">
                                     <img src="/img/slider/002.png" alt="slide7" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat">
-                                </li> 
+                                </li>
                                 <li data-transition="slideleft" data-slotamount="5" data-masterspeed="700" data-delay="4000">
                                     <img src="/img/slider/003.png" alt="slide7" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat">
-                                </li> 
+                                </li>
                                 <li data-transition="slideleft" data-slotamount="5" data-masterspeed="700" data-delay="4000">
                                     <img src="/img/slider/004.png" alt="slide7" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat">
-                                </li> 
+                                </li>
                                 <li data-transition="slideleft" data-slotamount="5" data-masterspeed="700" data-delay="4000">
                                     <img src="/img/slider/005.png" alt="slide7" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat">
-                                </li> 
+                                </li>
 
 
                                 <!-- SLIDE 2  -->
@@ -80,39 +80,39 @@
                                 <!-- SLIDE 3  -->
 <!--                                <li data-transition="slideleft" data-slotamount="5" data-masterspeed="700" data-delay="4000">
                                     <img src="/img/slider/img4-min.png" alt="slide3" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat">
-                                </li>                  
+                                </li>
 
-                                 SLIDE 4  
+                                 SLIDE 4
                                 <li data-transition="slideleft" data-slotamount="5" data-masterspeed="700" data-delay="4000">
                                     <img src="/img/slider/img5-min.png" alt="slide4" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat">
                                 </li>
 
-                                 SLIDE 5  
+                                 SLIDE 5
                                 <li data-transition="slideleft" data-slotamount="5" data-masterspeed="700" data-delay="4000">
                                     <img src="/img/slider/img6-min.png" alt="slide5" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat">
-                                </li>                  
+                                </li>
 
-                                 SLIDE 6  
+                                 SLIDE 6
                                 <li data-transition="slideleft" data-slotamount="5" data-masterspeed="700" data-delay="4000">
                                     <img src="/img/slider/img7-min.png" alt="slide6" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat">
-                                </li>    
+                                </li>
 
-                                 SLIDE 7  
+                                 SLIDE 7
                                 <li data-transition="slideleft" data-slotamount="5" data-masterspeed="700" data-delay="4000">
                                     <img src="/img/slider/img8-min.png" alt="slide7" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat">
-                                </li>                                
+                                </li>
 
-                                 SLIDE 8  
+                                 SLIDE 8
                                 <li data-transition="slideleft" data-slotamount="5" data-masterspeed="700" data-delay="4000">
                                     <img src="/img/slider/img9-min.png" alt="slide8" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat">
                                 </li>
 
-                                 SLIDE 1  
+                                 SLIDE 1
                                 <li data-transition="slideleft" data-slotamount="5" data-masterspeed="700" data-delay="4000">
                                     <img src="/img/slider/slide1-min.jpg" alt="slide1" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat">
                                 </li>                  -->
 
-                            </ul>          
+                            </ul>
                         </div>
 
 
@@ -121,18 +121,18 @@
                                                         <a href="http://www.midearussia.ru/newyearpromo/">
                                                             <img src="/img/baner1.gif" style="width: 230px;height: 350px;">
                                                         </a>
-                        
+
                                                     </object>
                                                 </div>-->
 
 
                         <!--                        <div class="advertising-img second"><object height="350" width="230" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,18,0" type="application/x-shockwave-flash" data="/img/skovorod_pod.swf">
-                                                        <param name="quality" value="high"> 
-                                                        <param name="scale" value="noborder"> 
-                                                        <param name="salign" value="t"> 
-                                                        <param name="wmode" value="opaque"> 
-                                                        <param name="src" value="/banners/simply.swf"> 
-                                                        <embed height="350" width="230" id="linebanner" src="/img/skovorod_pod.swf">  
+                                                        <param name="quality" value="high">
+                                                        <param name="scale" value="noborder">
+                                                        <param name="salign" value="t">
+                                                        <param name="wmode" value="opaque">
+                                                        <param name="src" value="/banners/simply.swf">
+                                                        <embed height="350" width="230" id="linebanner" src="/img/skovorod_pod.swf">
                                                     </object>
                                                 </div>-->
 
@@ -188,8 +188,8 @@
 <!--            <div class="bottom_scallop">&nbsp;</div>
 
             <div class="top_scallop">&nbsp;</div>-->
-            
-            
+
+
 <script type="text/javascript" src="http://previous.themepunch.com/revolution/liveguide/rs-plugin/js/jquery.themepunch.plugins.min.js"></script>
 <script type="text/javascript" src="http://previous.themepunch.com/revolution/liveguide/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
 <link rel="stylesheet" type="text/css" href="http://previous.themepunch.com/revolution/liveguide/rs-plugin/css/settings.css" media="screen" />
@@ -205,7 +205,7 @@ $(document).ready(function() {
          {
              var revapi;
              jQuery(document).ready(function () {
-                 revapi = jQuery('.slider-center > div').revolution( 
+                 revapi = jQuery('.slider-center > div').revolution(
                          {
                              delay: 9000,
                              startwidth: 900,
